59ce3e68af9310e517632b1d35851909897a5266
manthonyy <manthony@student.ciputra.ac.id>
2026-05-22 14:25:09 +0700

    Update PlasticWaste.java

--- alp/PlasticWaste.java
+++ alp/PlasticWaste.java
@@ -1,16 +1,11 @@
 public class PlasticWaste extends WasteItem {
-
     public PlasticWaste(double weight){
         super("Plastic Waste",weight,10);
     }
-
     @Override
     public void displayWasteInfo(){
-
         System.out.println("Plastic Waste");
-
         System.out.println("High Pollution Impact");
-
         System.out.println("Standard Recycling Process");
     }
 }
