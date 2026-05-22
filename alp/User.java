09f84bf902cc57f928f6829bb3f6e5bf33c85fa0
manthonyy <manthony@student.ciputra.ac.id>
2026-05-22 14:26:16 +0700

    Update User.java

--- alp/User.java
+++ alp/User.java
@@ -1,60 +1,40 @@
 import java.util.ArrayList;
-
 public class User {
-
     private String userId;
     private String username;
     private String password;
-
     private UserRole role;
-
     private int totalPoints;
     ArrayList<Transaction> transactionHistory = new ArrayList<>();
-    public User(
-            String userId,
-            String username,
-            String password,
-            UserRole role
-    ) {
-
+    public User(String userId, String username, String password, UserRole role) {
         this.userId = userId;
         this.username = username;
         this.password = password;
         this.role = role;
         totalPoints = 0;
     }
-
     public String getUserId() {
         return userId;
     }
-
     public String getUsername() {
         return username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public UserRole getRole() {
         return role;
     }
     public int getTotalPoints(){
         return totalPoints;
     }
-
     public void addPoints(int points) {totalPoints += points;}
-
     public void reducePoints(int points) {totalPoints -= points;}
     public void viewTransactionHistory(){
-
     if(transactionHistory.isEmpty()){
-
         System.out.println("No Transaction History");
-
     }
     else{
-
         for(Transaction t : transactionHistory){
             t.displayTransaction();
         }
