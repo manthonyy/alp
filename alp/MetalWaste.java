f6947dd6954105c5380e7d34377ac8ec78d8c3cf
manthonyy <manthony@student.ciputra.ac.id>
2026-05-22 14:24:38 +0700

    Update MetalWaste.java

--- alp/MetalWaste.java
+++ alp/MetalWaste.java
@@ -1,16 +1,11 @@
 public class MetalWaste extends WasteItem {
-
     public MetalWaste(double weight){
         super("Metal Waste",weight,20);
     }
-
     @Override
     public void displayWasteInfo(){
-
         System.out.println("Metal Waste");
-
         System.out.println("High Recycling Value");
-
         System.out.println("Fast Processing");
     }
 }
