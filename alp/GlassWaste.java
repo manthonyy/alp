1756626bb7afd104e61a6b702baa59c0f94697d1
manthonyy <manthony@student.ciputra.ac.id>
2026-05-22 14:21:16 +0700

    Update GlassWaste.java

--- alp/GlassWaste.java
+++ alp/GlassWaste.java
@@ -1,17 +1,11 @@
 public class GlassWaste extends WasteItem {
-
     public GlassWaste(double weight){
-
         super("Glass Waste",weight,8);
     }
-
     @Override
     public void displayWasteInfo(){
-
         System.out.println("Glass Waste");
-
         System.out.println("Low Pollution Impact");
-
         System.out.println("Longer Processing Time");
     }
 }
