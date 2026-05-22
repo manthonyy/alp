88993c7e2343950cf688b608a5d70b4870bf52c0
manthonyy <manthony@student.ciputra.ac.id>
2026-05-22 14:27:19 +0700

    Update WasteItem.java

--- alp/WasteItem.java
+++ alp/WasteItem.java
@@ -2,8 +2,7 @@ public abstract class WasteItem {
     protected String wasteName;
     protected double weight;
     protected int pointsPerKg;
-    public WasteItem(String wasteName,double weight,int pointsPerKg)
-    {
+    public WasteItem(String wasteName,double weight,int pointsPerKg){
         this.wasteName = wasteName;
         this.weight = weight;
         this.pointsPerKg = pointsPerKg;
